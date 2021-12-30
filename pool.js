0e63a0e6b15ef8c1e286e8f92d2a35b754942e8d
mattndross <mattndross@users.noreply.github.com>
2021-12-30 16:39:18 +0100

    modifica pool

--- pool.js
+++ pool.js
@@ -1,8 +1,8 @@
 const { Pool } = require('pg');
 exports.pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'proximity_network',
-    password: 'fran1985',
+    user: 'jsrrjrwfbkgzrf',
+    host: 'ec2-54-225-203-79.compute-1.amazonaws.com',
+    database: 'd125qk1gru47mv',
+    password: '96109793cab756bc0d0036b1efaa13469d7ddcb4916c4428c69a110cb383b986',
     port: 5432
 });
